1d0ac93e33c33aed4c7aa79f32c03b980b32a8e1
Pisit akarateerapisal <6110110320@psu.ac.th>
2022-01-24 18:37:07 +0700

    add with method post

--- src/main.js
+++ src/main.js
@@ -1,15 +1,18 @@
+const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 
+app.use(bodyParser.urlencoded({extended: false}))
 app.use('/static', express.static('public'))
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.get('/add', (req, res) => {
-    let a = parseInt(req.query.a)
-    let b = parseInt(req.query.b)
+app.post('/add', (req, res) => {
+    let a = parseInt(req.body.a)
+    let b = parseInt(req.body.b)
     res.send('' + (a + b))
 })
 
