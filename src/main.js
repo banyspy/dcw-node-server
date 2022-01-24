580fab965b55eedc7627ba96d4d1cc6906b78035
Pisit akarateerapisal <6110110320@psu.ac.th>
2022-01-24 18:32:38 +0700

    add with get method

--- src/main.js
+++ src/main.js
@@ -6,3 +6,14 @@ app.use('/static', express.static('public'))
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+app.get('/add', (req, res) => {
+    let a = parseInt(req.query.a)
+    let b = parseInt(req.query.b)
+    res.send('' + (a + b))
+})
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
+})
+  
