089d05556dcd980f4c99de109caca3e857b61b3b
Pisit akarateerapisal <6110110320@psu.ac.th>
2022-01-24 18:39:13 +0700

    add with json

--- src/main.js
+++ src/main.js
@@ -4,10 +4,18 @@ const app = express()
 const port = 3000
 
 app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
 app.use('/static', express.static('public'))
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/add', (req, res) => {
+    let a = parseInt(req.query.a)
+    let b = parseInt(req.query.b)
+    res.send('' + (a + b))
 })
 
 app.post('/add', (req, res) => {
@@ -16,7 +24,12 @@ app.post('/add', (req, res) => {
     res.send('' + (a + b))
 })
 
-app.listen(port, () => {
-    console.log(`Listening on port ${port}`)
+app.post('/add2', (req, res) => {
+  let a = req.body.a
+  let b = req.body.b
+  res.send({result: a + b})
 })
-  
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`)
+})
